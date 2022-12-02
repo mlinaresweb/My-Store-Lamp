28bc921536bea26248c86c1bf88cb180c6fa6c5c
g33k1e <jquerolmar@gmail.com>
2022-12-02 11:11:10 +0100

    route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+
 
 const routes: Routes = [
   {path:'',redirectTo:'AppComponent',pathMatch:'full'}
